1dd4e84ab01d0ef6ae6e4d0e764114b10e34bb3c
Vani <vani@tinkerlust.com>
2018-01-31 11:08:28 +0000

    add initial file

---
+++ js/less/init.js
@@ -0,0 +1,9 @@
+less = {
+    env: "development", // or "production"
+    async: true,        // load imports async
+    poll: 2000,         // when in watch mode, time in ms between polls
+    dumpLineNumbers: "all" // or "mediaQuery" or "all"
+//    relativeUrls: false // whether to adjust url's to be relative
+//                        // if false, url's are already relative to the
+//                        // entry less file
+};
